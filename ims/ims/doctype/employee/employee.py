46f0cdb431a0132248bad1e53241cde05d71020c
NehalkumarNrK <nehal.kumar@soulunileaders.com>
2022-10-25 17:52:29 +0530

    New user created when employee is created

--- ims/ims/doctype/employee/employee.py
+++ ims/ims/doctype/employee/employee.py
@@ -1,8 +1,29 @@
 # Copyright (c) 2022, SOUL and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Employee(Document):
-	pass
+	# pass
+	def after_insert(self):
+		self.create_emp_user()
+
+	def create_emp_user(self):
+		print("\n\n\n\n\n\n\n")
+		print("emp_user")
+		if not frappe.db.exists("User", self.email):
+			emp_user = frappe.get_doc({
+				'doctype':'User',
+				'first_name': self.first_name,
+				'last_name': self.last_name,
+				'email': self.email,
+				'gender': self.gender,
+				'send_welcome_email': 1,
+				'user_type': 'Website User'
+				})
+			print("\n\n\n\n\n\n\n")
+			print(emp_user)
+			emp_user.flags.ignore_permissions = True
+			emp_user.add_roles("Accounts Manager")
+			emp_user.save()
