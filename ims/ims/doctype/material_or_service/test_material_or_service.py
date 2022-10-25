e242d72c1920398f8621af51537cb0e64a32bb99
NehalkumarNrK <nehal.kumar@soulunileaders.com>
2022-10-25 17:22:41 +0530

    doctype

---
+++ ims/ims/doctype/material_or_service/test_material_or_service.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, SOUL and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMaterialorService(FrappeTestCase):
+	pass
