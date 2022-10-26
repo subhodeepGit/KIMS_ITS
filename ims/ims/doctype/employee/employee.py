7ce66f66a65714a95b156e7ffe6285ebe5d70dcd
NehalkumarNrK <nehal.kumar@soulunileaders.com>
2022-10-26 15:48:05 +0530

    user deleted through employee

--- ims/ims/doctype/employee/employee.py
+++ ims/ims/doctype/employee/employee.py
@@ -8,10 +8,16 @@ class Employee(Document):
 	# pass
 	def after_insert(self):
 		self.create_emp_user()
+	
+	def validate(self):
+		self.enabled_emp()
+		self.disable_emp()
+
+	def on_trash(self):
+		for usr in frappe.db.get_all("User", {'email':self.email},{'name'}):
+			frappe.delete_doc("User",usr.name)
 
 	def create_emp_user(self):
-		print("\n\n\n\n\n\n\n")
-		print("emp_user")
 		if not frappe.db.exists("User", self.email):
 			emp_user = frappe.get_doc({
 				'doctype':'User',
@@ -19,11 +25,30 @@ class Employee(Document):
 				'last_name': self.last_name,
 				'email': self.email,
 				'gender': self.gender,
+				'enabled': self.enabled,
 				'send_welcome_email': 1,
 				'user_type': 'Website User'
 				})
-			print("\n\n\n\n\n\n\n")
-			print(emp_user)
 			emp_user.flags.ignore_permissions = True
 			emp_user.add_roles("Accounts Manager")
 			emp_user.save()
+
+	def enabled_emp(self):
+		if self.enabled==0:
+			emp=frappe.db.get_all("User", {'email':self.email},['name','enabled'])
+			status=emp[0]['enabled']
+			emp_name = emp[0]['name']
+			if status == 1:
+				update_doc = frappe.get_doc("User",emp_name)
+				update_doc.enabled=0
+				update_doc.save()
+
+	def disable_emp(self):
+		if self.enabled==1:
+			emp=frappe.db.get_all("User", {'email':self.email},['name','enabled'])
+			status=emp[0]['enabled']
+			emp_name = emp[0]['name']
+			if status == 0:
+				update_doc = frappe.get_doc("User",emp_name)
+				update_doc.enabled=1
+				update_doc.save()
