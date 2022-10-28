e63fd3bf2251712e848db6a225e5ee99e30b3d55
NehalkumarNrK <nehal.kumar@soulunileaders.com>
2022-10-28 15:16:34 +0530

    Non PO Non Contact doctype

---
+++ ims/ims/doctype/non_po_contract/non_po_contract.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, SOUL and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Non PO Contract', {
+	// refresh: function(frm) {
+
+	// }
+});
