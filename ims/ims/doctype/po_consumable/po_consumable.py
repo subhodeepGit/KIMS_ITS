6167266c645bec7011b448e65e0be0b733689fde
NehalkumarNrK <nehal.kumar@soulunileaders.com>
2022-11-02 10:19:48 +0530

    update in Po_con

--- ims/ims/doctype/po_consumable/po_consumable.py
+++ ims/ims/doctype/po_consumable/po_consumable.py
@@ -1,8 +1,26 @@
 # Copyright (c) 2022, SOUL and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class POConsumable(Document):
-	pass
+	def validate(self):
+		print("\n\n\n\n\n\n")
+		session_user = frappe.session.user
+		print(session_user)
+		data = frappe.get_all("Employee",{"email":session_user},{"name"})
+		print(data)
+		for i in data:
+			self.append("authorized_signature",{                                     
+				"emp_id":i['name'],                                       
+				"emp_name":"Ram",                                        
+				"designation":"Dr.",                                        
+				"date_of_approval":"2022-11-02",                                        
+				"date_of_receivable":"2022-11-02",                                        
+				"department":"D",                                        
+				"approval_status":"Save",                                        
+				"previous_status":"Draft",                                        
+				"transfer_to":0,                                    
+			})
+			a.s
