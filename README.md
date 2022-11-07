f507680a3fa8bd5ce93f8924fb80442173ae403e
deysubhodeep <subhodeep.dey@soulunileaders.com>
2022-11-07 13:33:28 +0530

    merged 1

--- README.md
+++
@@ -1,7 +0,0 @@
-## Ims
-
-IMS
-
-#### License
-
-MIT
--- ims/ims/doctype/po_material_management/po_material_management.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022, SOUL and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('PO Material Management', {
-	// refresh: function(frm) {
-
-	// }
-});
