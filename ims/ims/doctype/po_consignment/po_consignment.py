8dbaff4a81898a93a1a939b914e825c99c810113
deysubhodeep <subhodeep.dey@soulunileaders.com>
2022-11-07 15:54:04 +0530

    workflow logic

--- ims/ims/doctype/po_consignment/po_consignment.py
+++ ims/ims/doctype/po_consignment/po_consignment.py
@@ -1,8 +1,92 @@
 # Copyright (c) 2022, SOUL and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe import utils
 
 class POConsignment(Document):
-	pass
+	def validate(self):
+		session_user = frappe.session.user
+		if self.workflow_state!="Cancelled" and self.workflow_state!="Rejected and Transfer":
+			if session_user:
+				emp_data = frappe.get_all("Employee",{"email":session_user,"enabled":1},["name","full_name","salutation","designation","department"])
+				if emp_data:
+					flag="Yes"
+					object_var=""
+					for t in self.get("authorized_signature"):
+						object_var=t
+
+					if object_var!="":
+						if object_var.approval_status==self.workflow_state :
+							flag="No"
+
+					approval_status=self.workflow_state
+					previous_status=frappe.get_all("PO Consignment",{"name":self.name},["workflow_state"])
+					if not previous_status:
+						previous_status=""
+					else:
+						previous_status=previous_status[0]['workflow_state']	
+					
+					date_of_receivable=""
+					for t in self.get("authorized_signature"):
+						date_of_receivable=t.date_of_approval
+					
+					if date_of_receivable=="":
+						date_of_receivable=utils.today()
+					emp_name=emp_data[0]['salutation']+" "+emp_data[0]['full_name']
+
+
+					if flag=="Yes":
+						self.append("authorized_signature",{                                     
+							"emp_id":emp_data[0]['name'],                                       
+							"emp_name":emp_name,                                        
+							"designation":emp_data[0]['designation'],                                        
+							"date_of_approval":utils.today(),                                        
+							"date_of_receivable":date_of_receivable,                                        
+							"department":emp_data[0]['department'],                                        
+							"approval_status":approval_status,                              
+							"previous_status":previous_status,                                 
+							"transfer_to":0,                    
+						})
+					if flag=="No":
+						for t in self.get("authorized_signature"):
+							if t.name==object_var.name:
+								previous_status=t.previous_status
+								t.emp_id=emp_data[0]['name']
+								t.emp_name=emp_name
+								t.designation=emp_data[0]['designation']
+								t.date_of_approval=utils.today()
+								t.date_of_receivable=date_of_receivable
+								t.department=emp_data[0]['department']
+								t.approval_status=approval_status
+								t.previous_status=previous_status
+
+			else:
+				frappe.throw("Employee not found")		
+		else:
+			if self.workflow_state=="Rejected and Transfer":
+				check=""
+				count=0
+				name=""
+				for t in self.get("authorized_signature"):
+					if t.transfer_to==1 and t.disapproval_check==0:
+						check=t.previous_status
+						name=t.name
+				if name!="":
+					emp_data = frappe.get_all("Employee",{"email":session_user,"enabled":1},["name","full_name","salutation","designation","department"])
+					if emp_data:
+						emp_name=emp_data[0]['salutation']+" "+emp_data[0]['full_name']
+						for t in self.get("authorized_signature"):
+							if t.name>=name:
+								frappe.db.sql(""" update `tabAuthorized Signature` set disapproval_check=1,disapproval_emp_name="%s",disapproval_emp="%s" 
+												where name="%s" """%(emp_name,emp_data[0]['name'],t.name))
+								frappe.db.commit()
+
+						frappe.db.sql(""" update `tabPO Consignment` set workflow_state="%s" where name="%s" """%(check,self.name))
+						frappe.db.commit()
+						frappe.throw("Your Rejection and Transfer is Completed, So Please Referesh your page")	
+					else:
+						frappe.throw("Employee Not Found")			
+				else:
+					frappe.throw("Transfer To Employee Not Selected")
