dc75866c4112e04002841618961b8b011ee60e0a
deysubhodeep <subhodeep.dey@soulunileaders.com>
2022-11-08 15:30:55 +0530

    print format and doctype logic

--- ims/ims/doctype/po_consignment/po_consignment.js
+++ ims/ims/doctype/po_consignment/po_consignment.js
@@ -6,29 +6,55 @@ frappe.ui.form.on('PO Consignment', {
 
 	// }
 });
+
 frappe.ui.form.on('Credit Note and PO', {	//Child table Name
 	invoice_amount:function(frm, cdt, cdn){	//Child table field Name where you data enter
 	var d = locals[cdt][cdn];
 	var total = 0;
 	let a= parseInt(total)
-	frm.doc.details_of_invoices_credit_note_and_po.forEach(function(d)  { a = a+ d.invoice_amount; }); //Child table name and field name
+	frm.doc.details_of_invoices_credit_note_and_po.forEach(function(d)  { a = a+ d.invoice_amount;}); //Child table name and field name
 	frm.set_value("total_amount_in_rs", a);			// Parent field name where calculation going to fetch
 	refresh_field("total_amount_in_rs");
-	frm.set_value("net_final_amount_to_be_paid_in_rs",a) // Parent field name where calculation going to fetch
+	frm.set_value("net_final_amount_to_be_paid_in_rs", a); // Parent field name where calculation going to fetch
 	refresh_field("net_final_amount_to_be_paid_in_rs");
-  },
+   },
   // When field is deleted from child table on that time calculation going to happend to the accordingly
-  details_of_invoices_credit_note_and_po_remove:function(frm, cdt, cdn){ //Child table name
+    details_of_invoices_credit_note_and_po_remove:function(frm, cdt, cdn){ //Child table name
 	var d = locals[cdt][cdn];
 	var total = 0;
-	let a= parseInt(total)
-	frm.doc.details_of_invoices_credit_note_and_po.forEach(function(d) { a += d.invoice_amount; });
+	let a= parseInt(total);
+	var total1 = 0;
+	let b= parseInt(total1);
+	var total2 = 0;
+	let c= parseInt(total2);
+	frm.doc.details_of_invoices_credit_note_and_po.forEach(function(d) { a += d.invoice_amount; b += d.hospital_margin_amount; c += d.to_pay;});
 	frm.set_value("total_amount_in_rs", a);
 	refresh_field("total_amount_in_rs");
 	frm.set_value("net_final_amount_to_be_paid_in_rs",a)
 	refresh_field("net_final_amount_to_be_paid_in_rs");
-	}
+	frm.set_value("total_hospital_margin_amount",b)
+	refresh_field("total_hospital_margin_amount");
+	frm.set_value("to_pay_total",c)
+	refresh_field("to_pay_total");
+	},
+	hospital_margin_amount:function(frm, cdt, cdn){	//Child table field Name where you data enter
+		var d = locals[cdt][cdn];
+		var total = 0;
+		let a= parseInt(total)
+		frm.doc.details_of_invoices_credit_note_and_po.forEach(function(d)  { a = a+ d.hospital_margin_amount;}); //Child table name and field name
+		frm.set_value("total_hospital_margin_amount", a);			
+		refresh_field("total_hospital_margin_amount");
+	},
+	to_pay:function(frm, cdt, cdn){	//Child table field Name where you data enter
+		var d = locals[cdt][cdn];
+		var total = 0;
+		let b= parseInt(total)
+		frm.doc.details_of_invoices_credit_note_and_po.forEach(function(d)  { b = b+ d.to_pay;}); //Child table name and field name
+		frm.set_value("to_pay_total", b);			
+		refresh_field("to_pay_total");
+	},
 });
+
 
 frappe.ui.form.on('PO Consignment', {
 	total_amount_in_rs: function(frm) {
