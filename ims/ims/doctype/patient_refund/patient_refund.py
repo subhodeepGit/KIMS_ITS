19256c83ae37ceeeba37cbc751fad88068865489
NehalkumarNrK <nehal.kumar@soulunileaders.com>
2022-11-10 16:33:58 +0530

    money in word validation

--- ims/ims/doctype/patient_refund/patient_refund.py
+++ ims/ims/doctype/patient_refund/patient_refund.py
@@ -1,8 +1,10 @@
 # Copyright (c) 2022, SOUL and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import money_in_words
 
 class PatientRefund(Document):
-	pass
+	def validate(self):
+		self.net_refundable_in_words = money_in_words(self.net_refundable_in_figures)
