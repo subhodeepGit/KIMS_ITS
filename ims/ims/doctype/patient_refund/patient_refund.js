76cf636171eb948a077e314d1e8cd3e8a1bb1fb5
NehalkumarNrK <nehal.kumar@soulunileaders.com>
2022-11-11 12:32:22 +0530

    Read only full name and patient validation

--- ims/ims/doctype/patient_refund/patient_refund.js
+++ ims/ims/doctype/patient_refund/patient_refund.js
@@ -1,8 +1,38 @@
 // Copyright (c) 2022, SOUL and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Patient Refund', {
-	// refresh: function(frm) {
-
-	// }
+frappe.ui.form.on('Patient Refund', "amount_deposited_by_patient", function(frm) {
+		var net_amount = 0;
+		net_amount = cur_frm.doc.amount_deposited_by_patient - cur_frm.doc.approval_of_tpa__insurance__corporate__ostf - cur_frm.doc.cash_refund - cur_frm.doc.total_bill - cur_frm.doc.less__non_admissible_item__discount_amount
+		frm.set_value("net_refundable_in_figures",net_amount)
+		refresh_field("net_refundable_in_figures");
+		
+});
+frappe.ui.form.on('Patient Refund', "approval_of_tpa__insurance__corporate__ostf", function(frm) {
+	var net_amount = 0;
+	net_amount = cur_frm.doc.amount_deposited_by_patient - cur_frm.doc.approval_of_tpa__insurance__corporate__ostf - cur_frm.doc.cash_refund - cur_frm.doc.total_bill - cur_frm.doc.less__non_admissible_item__discount_amount
+	frm.set_value("net_refundable_in_figures",net_amount)
+	refresh_field("net_refundable_in_figures");
+	
+});
+frappe.ui.form.on('Patient Refund', "cash_refund", function(frm) {
+	var net_amount = 0;
+	net_amount = cur_frm.doc.amount_deposited_by_patient - cur_frm.doc.approval_of_tpa__insurance__corporate__ostf - cur_frm.doc.cash_refund - cur_frm.doc.total_bill - cur_frm.doc.less__non_admissible_item__discount_amount
+	frm.set_value("net_refundable_in_figures",net_amount)
+	refresh_field("net_refundable_in_figures");
+	
+});
+frappe.ui.form.on('Patient Refund', "total_bill", function(frm) {
+	var net_amount = 0;
+	net_amount = cur_frm.doc.amount_deposited_by_patient - cur_frm.doc.approval_of_tpa__insurance__corporate__ostf - cur_frm.doc.cash_refund - cur_frm.doc.total_bill - cur_frm.doc.less__non_admissible_item__discount_amount
+	frm.set_value("net_refundable_in_figures",net_amount)
+	refresh_field("net_refundable_in_figures");
+	
+});
+frappe.ui.form.on('Patient Refund', "less__non_admissible_item__discount_amount", function(frm) {
+	var net_amount = 0;
+	net_amount = cur_frm.doc.amount_deposited_by_patient - cur_frm.doc.approval_of_tpa__insurance__corporate__ostf - cur_frm.doc.cash_refund - cur_frm.doc.total_bill - cur_frm.doc.less__non_admissible_item__discount_amount
+	frm.set_value("net_refundable_in_figures",net_amount)
+	refresh_field("net_refundable_in_figures");
+	
 });
--- ims/ims/doctype/patient_refund/patient_refund.py
+++ ims/ims/doctype/patient_refund/patient_refund.py
@@ -7,5 +7,5 @@ from frappe.utils import money_in_words
 
 class PatientRefund(Document):
 	def validate(self):
-		self.net_refundable_in_figures=(self.amount_deposited_by_patient - self.approval_of_tpa__insurance__corporate__ostf - self.cash_refund - self.total_bill - self.approval_of_tpa__insurance__corporate__ostf - self.less__non_admissible_item__discount_amount)
+		# self.net_refundable_in_figures=(self.amount_deposited_by_patient - self.approval_of_tpa__insurance__corporate__ostf - self.cash_refund - self.total_bill - self.approval_of_tpa__insurance__corporate__ostf - self.less__non_admissible_item__discount_amount)
 		self.net_refundable_in_words = money_in_words(self.net_refundable_in_figures)
