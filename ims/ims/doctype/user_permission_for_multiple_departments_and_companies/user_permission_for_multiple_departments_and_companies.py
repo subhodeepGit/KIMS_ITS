e6a5f32e95c9f950db09ce8de232afa4930fe0cf
deysubhodeep <subhodeep.dey@soulunileaders.com>
2022-11-16 16:36:12 +0530

    multiple user perm companies

--- ims/ims/doctype/user_permission_for_multiple_departments_and_companies/user_permission_for_multiple_departments_and_companies.py
+++ ims/ims/doctype/user_permission_for_multiple_departments_and_companies/user_permission_for_multiple_departments_and_companies.py
@@ -59,4 +59,54 @@ class UserPermissionforMultipleDepartmentsandCompanies(Document):
 		else:
 			frappe.throw("Email id not found")
 
+######Company User Perm
+		if self.disable==1:
+			filter=[]
+			filter.append(["user","=",emp_email])
+			filter.append(["allow","=","Company"])
+			dep_info=frappe.get_all("User Permission",filters=filter,fields=["name"])
+			for t in dep_info:
+				frappe.delete_doc("User Permission", t['name'])
+		if emp_email :
+			if self.disable==0:
 
+				filter=[]
+				filter.append(["user","=",emp_email])
+				filter.append(["allow","=","Company"])
+				dep_info=frappe.get_all("User Permission",filters=filter,fields=["name"])
+				for t in dep_info:
+					frappe.delete_doc("User Permission", t['name'])
+				parent_dept=[]
+				dept_info=[]
+
+				for t in self.get("companies"):
+					parent_dept.append(t.company)
+					# dept_info.append(t.department)
+				parent_dept=list(set(parent_dept))
+				dept_info=list(set(dept_info))
+				###### for parent dept
+				for t in parent_dept:
+					frappe.get_doc(
+						dict(
+							doctype="User Permission",
+							user=emp_email,
+							allow="Company",
+							for_value=t,
+							apply_to_all_doctypes=1,
+							hide_descendants=1,
+						)
+					).insert(ignore_permissions=False)
+				######## for clild
+				for t in dept_info:
+					frappe.get_doc(
+									dict(
+										doctype="User Permission",
+										user=emp_email,
+										allow="Company",
+										for_value=t,
+										apply_to_all_doctypes=1,
+										hide_descendants=0,
+									)
+								).insert(ignore_permissions=False)
+		else:
+			frappe.throw("Email id not found")
