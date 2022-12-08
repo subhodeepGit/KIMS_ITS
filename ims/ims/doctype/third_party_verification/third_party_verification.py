92e1419d9d9eddb63f419d287c04e9a8afd91668
NehalkumarNrK <nehal.kumar@soulunileaders.com>
2022-12-08 18:19:50 +0530

    update in third party

--- ims/ims/doctype/third_party_verification/third_party_verification.py
+++ ims/ims/doctype/third_party_verification/third_party_verification.py
@@ -16,21 +16,3 @@ class ThirdPartyVerification(Document):
 				break
 		frappe.db.set_value("Reference of  Note sheet Verification",name,"final_status","Close")
 		frappe.db.set_value("Reference of  Note sheet Verification",name,"status_of_verification","Verification Completed")		
-
-		# if name_doc
-		# third_party_child_data=frappe.get_all("")
-		# a.s
-		# if name_doc == "PO Consumable":
-		# 	third_party_child_data=frappe.get_all("Reference of  Note sheet Verification",{"parent":""})
-		# elif name_doc== "PO Consignment":
-		# 	attachments = [frappe.attach_print(self.doctype, self.name, file_name=self.name, print_format='PO Consignment PF')]
-		# elif name_doc == "PO Material Management":
-		# 	attachments = [frappe.attach_print(self.doctype, self.name, file_name=self.name, print_format='PO Material Management PF')]
-		# elif name_doc == "Pharmacy":
-		# 	attachments = [frappe.attach_print(self.doctype, self.name, file_name=self.name, print_format='Pharmacy PF')]
-		# elif name_doc== "Non PO Contract":
-		# 	attachments = [frappe.attach_print(self.doctype, self.name, file_name=self.name, print_format='Non Po Contract PF')]
-		# elif name_doc == "Non PO Non Contract":
-		# 	attachments = [frappe.attach_print(self.doctype, self.name, file_name=self.name, print_format='Non PO Non Contract PF')]
-		# elif name_doc == "Patient Refund":
-		# 	attachments = [frappe.attach_print(self.doctype, self.name, file_name=self.name, print_format='Patient Refund PF')]
