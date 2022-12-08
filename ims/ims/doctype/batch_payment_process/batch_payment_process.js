dfc2593fa35c3b481afb30a7675783f31521b373
deysubhodeep <subhodeep.dey@soulunileaders.com>
2022-12-08 16:23:58 +0530

    pf rename bug

--- ims/ims/doctype/batch_payment_process/batch_payment_process.js
+++ ims/ims/doctype/batch_payment_process/batch_payment_process.js
@@ -33,7 +33,7 @@ frappe.ui.form.on("Batch Payment Process", {
 		frm.set_df_property("table_26", "cannot_add_rows", true);
 	}
 });
-frappe.ui.form.on("Batch Payment Process", {
+frappe.ui.form.on("Batch Payment Process", { 
 	onload:function(frm){
 	if(frm.doc.document_status=="Payment Done"){
 		frm.set_df_property("vendor_wise_payment_details",'read_only', 1)
