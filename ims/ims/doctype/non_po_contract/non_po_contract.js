0006f44a7e1f5f6a77de62335fbcbb8e1ba5643d
NehalkumarNrK <nehal.kumar@soulunileaders.com>
2022-12-11 19:01:19 +0530

    update in 3rd party

--- ims/ims/doctype/non_po_contract/non_po_contract.js
+++ ims/ims/doctype/non_po_contract/non_po_contract.js
@@ -244,3 +244,39 @@ frappe.ui.form.on('Non PO Contract', {
         })
 	},
 });	
+frappe.ui.form.on('Non PO Contract', {
+	refresh: function(frm) {
+		if(frm.is_new()!=1){
+			frappe.call({
+				method: "ims.ims.doctype.non_po_contract.non_po_contract.get_action_acess",
+				args: {
+					self:frm.doc
+				},
+				callback: function(r) { 
+					if (r.message!=0){
+						$('.actions-btn-group').hide();
+						$('.primary-action').prop('hidden', true);
+						frm.refresh();
+					}
+					
+				}
+			})
+		}
+		// $('.primary-action').prop('hidden', true); //hide save button
+		// $('.actions-btn-group').hide(); //hide all button
+		}
+});
+
+
+frappe.ui.form.on('Non PO Contract', {
+	setup: function(frm) {
+		frm.set_query("employee_id", "third_party_verification", function(doc, cdt, cdn) {
+			let d = locals[cdt][cdn];
+			return {
+				filters: [
+					['Employee', 'third_party_employee', '=', 1],
+				]
+			};
+		});
+	},
+});
--- ims/ims/doctype/non_po_contract/non_po_contract.py
+++ ims/ims/doctype/non_po_contract/non_po_contract.py
@@ -10,43 +10,7 @@ class NonPOContract(Document):
 	def validate(self):
 
 		mandatory_check(self)
-
-		third_party_verification=self.get("third_party_verification")
-		if third_party_verification:
-			for t in self.get("third_party_verification"):
-				if t.final_status=="Open" and t.status_of_verification==None:
-					ref_party_doc=frappe.get_doc({
-						'doctype':'Third-Party Verification',
-						"company":self.company,
-						"type_of_note_sheet":"PO Consignment",
-						"documnet_no":self.name,
-						"note_sheet_no":self.note_sheet_no,
-						"date_of_note_sheet":self.date_of_note_sheet,
-						"name_of_schooldepartment":self.name_of_schooldepartment,
-						"for_which_department":self.for_which_department,
-						"document_status":self.document_status,
-						"priority":self.priority,
-					})
-					for j in self.get("details_of_invoices_and_po"):
-						ref_party_doc.append("third_party_verification_child",{
-							"details_of_invoices_po":j.details_of_invoices_and_po,
-							"invoice_no":j.invoice_no,
-							"invoice_date":j.invoice_date,
-							"invoices_amountin_rs":j.invoices_amountin_rs,
-							"invoice_attachment":j.invoice_attachment,
-							"credit_memo_attachment":j.credit_memo_attachment,
-							"document_attachment":j.document_attachment,
-							"po_attachment_attachment":j.po_attachment,
-							"bill_summary_attadelivery_challanchment":j.bill_summary,
-							"delivery_challan_attachment":j.delivery_challan,
-							"grn_attachment":j.grn_attachment,
-						})
-					ref_party_doc.save()
-					doc_name=ref_party_doc.name
-					t.status_of_verification="Forward For Verification"
-					t.document_name=doc_name
-
-
+		third_party_verification(self)
 		if self.workflow_state=="Bill Received by Audit":
 			date=datetime.date.today()
 			self.db_set("today_date",date)
@@ -186,7 +150,6 @@ def mandatory_check(self):
 	# 		count=count+1
 	# 		# if t.po_attachment_attachment!=1: 
 	# 		# 	frappe.throw("PO Attachment	is mandatory in row on %s"%(count))
-	
 	if self.workflow_state=="Bill Received by Audit" and (self.audit_ref_no==None or self.audit_ref_no==""): 
 		frappe.throw("Audit Ref No.	is mandatory")
 
@@ -218,3 +181,69 @@ def get_table_attachments():
 			flag['chk_fieldname']=a[0]
 			attachments.append(flag)	
 	return attachments
+
+def third_party_verification(self):
+	third_party_verification=self.get("third_party_verification")
+	if third_party_verification:
+		for t in self.get("third_party_verification"):
+			if t.final_status=="Open" and t.status_of_verification==None:
+				########################### proving role for creation
+				session_user = frappe.session.user
+				emp_data = frappe.get_all("Employee",{"email":session_user,"enabled":1},["designation","email"])
+				if emp_data:
+					###################### Data enrty in Third-Party Verification
+					ref_party_doc=frappe.get_doc({
+						'doctype':'Third-Party Verification',
+						"company":self.company,
+						"type_of_note_sheet":"Non PO Contract",
+						"documnet_no":self.name,
+						"note_sheet_no":self.note_sheet_no,
+						"date_of_note_sheet":self.date_of_note_sheet,
+						"name_of_schooldepartment":self.name_of_schooldepartment,
+						"for_which_department":self.for_which_department,
+						"document_status":self.document_status,
+						"priority":self.priority,
+						"approver_comment_to_third_party":t.remarks_by_creater
+					})
+					for j in self.get("details_of_enclosed_bills"):
+						ref_party_doc.append("third_party_verification_child",{
+							# "details_of_invoices_po":j.details_of_invoices_and_po,
+							"invoice_no":j.inv_no,
+							"invoice_date":j.invoice_date,
+							"invoices_amountin_rs":j.invoice_amount,
+							"invoice_attachment":j.invoice,
+							"credit_memo_attachment":j.credit_memo,
+							"document_attachment":j.others,
+							"po_attachment_attachment":j.po,
+							"bill_summary_attadelivery_challanchment":j.bill_challan,
+							"delivery_challan_attachment":j.delivery_challan,
+							"grn_attachment":j.grn,
+						})
+					ref_party_doc.save()
+					doc_name=ref_party_doc.name
+					t.status_of_verification="Forward For Verification"
+					t.document_name=doc_name
+					##################################### 3rd party Auto share with
+					user=t.employee_id
+					emp_data = frappe.get_all("Employee",{"name":user},["email"])
+					user=emp_data[0]["email"]
+					frappe.share.add_docshare('Third-Party Verification', 
+										doc_name, 
+										user, 
+										submit=1,
+										read=1,
+										write=1,    
+										flags={"ignore_share_permission": True}) 
+				else:
+					frappe.throw("Employee Not Found")	
+
+@frappe.whitelist()
+def get_action_acess(self):
+	import json
+	self = json.loads(self)
+	count=0
+	if self:
+		for t in self["third_party_verification"]:
+			if t['final_status']=="Open":
+				count=count+1
+	return count
