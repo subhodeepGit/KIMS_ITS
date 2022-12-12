f1d372b81ccdfb3e5a40dad0e0a682a57a1ebe06
NehalkumarNrK <nehal.kumar@soulunileaders.com>
2022-12-12 14:52:20 +0530

    Update in Rejset and trasfer bug

--- ims/ims/doctype/non_po_non_contract/non_po_non_contract.py
+++ ims/ims/doctype/non_po_non_contract/non_po_non_contract.py
@@ -17,6 +17,15 @@ class NonPONonContract(Document):
 			if session_user:
 				emp_data = frappe.get_all("Employee",{"email":session_user,"enabled":1},["name","full_name","salutation","designation","department"])
 				if emp_data:
+					##################### Rejected and Transfer Check
+					for t in self.get("authorized_signature"):
+						doc_before_save = self.get_doc_before_save()
+						if doc_before_save.document_status!=self.document_status:
+							if t.transfer_to==1 and t.disapproval_check==1:
+								pass
+							if t.transfer_to==1 and t.disapproval_check==0:
+								frappe.throw("Rejected and Transfer to state <b>%s</b> is checked in line no :-<b> %s </b> for the table Authorized Signature."%(t.approval_status,t.idx))
+					##############################
 					flag="Yes"
 					object_var=""
 					for t in self.get("authorized_signature"):
@@ -125,6 +134,8 @@ class NonPONonContract(Document):
 								frappe.db.commit()
 
 						frappe.db.sql(""" update `tabNon PO Non Contract` set workflow_state="%s" where name="%s" """%(check,self.name))
+						frappe.db.commit()
+						frappe.db.sql(""" update `tabNon PO Non Contract` set document_status="%s" where name="%s" """%("Rejected and Transfer",self.name))
 						frappe.db.commit()
 						frappe.throw("Your Rejection and Transfer is Completed, So Please Referesh your page")	
 					else:
