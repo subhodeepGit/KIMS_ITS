d96212389e6e5081b74d6acf5e494b02041066ed
NehalkumarNrK <nehal.kumar@soulunileaders.com>
2022-12-12 16:27:24 +0530

    New feature of the patient refund

---
+++ ims/ims/doctype/health_insurance_name/health_insurance_name.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, SOUL and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Health Insurance Name', {
+	// refresh: function(frm) {
+
+	// }
+});
