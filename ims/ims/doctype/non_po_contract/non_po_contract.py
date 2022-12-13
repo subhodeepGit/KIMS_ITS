ea9ce85b2e371f3a8d2f8bab9458951de8e0cfad
NehalkumarNrK <nehal.kumar@soulunileaders.com>
2022-12-13 13:41:08 +0530

    update in datatime in print format

--- ims/ims/doctype/non_po_contract/non_po_contract.py
+++ ims/ims/doctype/non_po_contract/non_po_contract.py
@@ -99,7 +99,7 @@ class NonPOContract(Document):
 						date_of_receivable=t.date_of_approval
 					
 					if date_of_receivable=="":
-						date_of_receivable=utils.today()
+						date_of_receivable=utils.now()
 					emp_name=emp_data[0]['salutation']+" "+emp_data[0]['full_name']
 
 
@@ -108,7 +108,7 @@ class NonPOContract(Document):
 							"emp_id":emp_data[0]['name'],                                       
 							"emp_name":emp_name,                                        
 							"designation":emp_data[0]['designation'],                                        
-							"date_of_approval":utils.today(),                                        
+							"date_of_approval":utils.now(),                                        
 							"date_of_receivable":date_of_receivable,                                        
 							"department":emp_data[0]['department'],                                        
 							"approval_status":approval_status,                              
@@ -125,7 +125,7 @@ class NonPOContract(Document):
 								t.emp_id=emp_data[0]['name']
 								t.emp_name=emp_name
 								t.designation=emp_data[0]['designation']
-								t.date_of_approval=utils.today()
+								t.date_of_approval=utils.now()
 								t.date_of_receivable=date_of_receivable
 								t.department=emp_data[0]['department']
 								t.approval_status=approval_status
--- ims/ims/doctype/pharmacy/pharmacy.py
+++ ims/ims/doctype/pharmacy/pharmacy.py
@@ -101,7 +101,7 @@ class Pharmacy(Document):
 						date_of_receivable=t.date_of_approval
 					
 					if date_of_receivable=="":
-						date_of_receivable=utils.today()
+						date_of_receivable=utils.now()
 					emp_name=emp_data[0]['salutation']+" "+emp_data[0]['full_name']
 
 
@@ -110,7 +110,7 @@ class Pharmacy(Document):
 							"emp_id":emp_data[0]['name'],                                       
 							"emp_name":emp_name,                                        
 							"designation":emp_data[0]['designation'],                                        
-							"date_of_approval":utils.today(),                                        
+							"date_of_approval":utils.now(),                                        
 							"date_of_receivable":date_of_receivable,                                        
 							"department":emp_data[0]['department'],                                        
 							"approval_status":approval_status,                              
@@ -127,7 +127,7 @@ class Pharmacy(Document):
 								t.emp_id=emp_data[0]['name']
 								t.emp_name=emp_name
 								t.designation=emp_data[0]['designation']
-								t.date_of_approval=utils.today()
+								t.date_of_approval=utils.now()
 								t.date_of_receivable=date_of_receivable
 								t.department=emp_data[0]['department']
 								t.approval_status=approval_status
