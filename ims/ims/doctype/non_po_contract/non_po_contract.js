9480cdd0a3e19d4dbf66ecbc759ffdac5bfc6f58
NehalkumarNrK <nehal.kumar@soulunileaders.com>
2022-12-14 13:00:32 +0530

    Non po contract invoices

--- ims/ims/doctype/non_po_contract/non_po_contract.js
+++ ims/ims/doctype/non_po_contract/non_po_contract.js
@@ -280,3 +280,17 @@ frappe.ui.form.on('Non PO Contract', {
 		});
 	},
 });
+
+frappe.ui.form.on('Non PO Contract', {
+	setup:function(frm) { 
+		frm.set_query("invoice_receival_no","details_of_enclosed_bills", function(_doc, cdt, cdn) {
+			return {
+			filters: [
+				["supplier_code",'=', frm.doc.supplier_code],
+				['Invoice Receival', 'invoice_status', '=', "Passed for Notesheet"]
+			],
+			};   
+		}); 
+	},
+	
+});
