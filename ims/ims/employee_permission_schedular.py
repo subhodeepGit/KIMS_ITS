03c50d99e1f93d5f57941845c8cd16c05c15d933
NehalkumarNrK <nehal.kumar@soulunileaders.com>
2023-01-05 15:32:38 +0530

    sdlur for role

--- ims/ims/employee_permission_schedular.py
+++ ims/ims/employee_permission_schedular.py
@@ -6,24 +6,25 @@ def employee_user():
     today = now.strftime("%Y-%m-%d %H:%M:%S")
     absent = frappe.get_all("Absent Employee Replacement",{"docstatus":1},["from_employee","email","role","from_date","to_employee","email1","to_date"])
     cancel = frappe.get_all("Absent Employee Replacement",{"docstatus":2},["from_employee","email","role","from_date","to_employee","email1","to_date"])
+    for Nr in cancel:
+        employee = frappe.get_doc("User",Nr["email1"])
+        if Nr:
+            employee.remove_roles(Nr["role"])
+            employee.flags.ignore_permissions = True
+            employee.save()
+        employee.save()
     for Nr in absent:
+        employee = frappe.get_doc("User",Nr["email1"])
         if Nr:
             from_dt = datetime.strftime(Nr["from_date"], "%Y-%m-%d %H:%M:%S")
             to_dt = datetime.strftime(Nr["to_date"], "%Y-%m-%d %H:%M:%S")
             if Nr["email"] != Nr["email1"]:
                 if from_dt <= today <= to_dt:
-                    employee = frappe.get_doc("User",Nr["email1"])
                     employee.add_roles(Nr["role"])
                     employee.flags.ignore_permissions = True
                     employee.save()
-                else:
-                    employee = frappe.get_doc("User",Nr["email1"])
+                if to_dt <= today:
                     employee.remove_roles(Nr["role"])
                     employee.flags.ignore_permissions = True
                     employee.save()
-    for Nr in cancel:
-        if Nr:
-            employee = frappe.get_doc("User",Nr["email1"])
-            employee.remove_roles(Nr["role"])
-            employee.flags.ignore_permissions = True
-            employee.save()
+        employee.save()
